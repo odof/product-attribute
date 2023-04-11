a10074657119d27390acb002ef5074b5414859a9
Cedric Le Brouster <cedric.lebrouster@openfire.fr>
2023-04-11 09:30:56 +0200

    [IMP] product_multi_image: follow base_multi_image rework

--- product_multi_image/__manifest__.py
+++ product_multi_image/__manifest__.py
@@ -12,7 +12,6 @@
     "license": "AGPL-3",
     "website": "https://github.com/OCA/product-attribute",
     "category": "Sales Management",
-    "pre_init_hook": "pre_init_hook",
     "uninstall_hook": "uninstall_hook",
     "depends": [
         "base_multi_image",
--- product_multi_image/hooks.py
+++ product_multi_image/hooks.py
@@ -6,17 +6,9 @@ _logger = logging.getLogger(__name__)
 
 
 try:
-    from odoo.addons.base_multi_image.hooks import (
-        pre_init_hook_for_submodules,
-        uninstall_hook_for_submodules,
-    )
+    from odoo.addons.base_multi_image.hooks import uninstall_hook_for_submodules
 except ImportError:
     _logger.info("Cannot import base_multi_image hooks")
-
-
-def pre_init_hook(cr):
-    pre_init_hook_for_submodules(cr, "product.template", "image_1920")
-    pre_init_hook_for_submodules(cr, "product.product", "image_variant_1920")
 
 
 def uninstall_hook(cr, registry):
